83274600f370121c750daf5c557043c01958d91c
Sai Prakash <saiprakash1916@gmail.com>
2025-12-16 09:45:31 +0530

    Modified and added one class

--- src/main/java/Basic_Coding_Questions/StringAndNumbers/palindromeCheck.java
+++ src/main/java/Basic_Coding_Questions/StringAndNumbers/palindromeCheck.java
@@ -1,6 +1,6 @@
-package interviewPoint;
+package Basic_Coding_Questions.StringAndNumbers;
 
-import java.util.Scanner;
+/* Write a program to check palindrome for both string and integer using one method. */
 
 /*
 Scanner reads everything as String.
@@ -9,12 +9,13 @@ We compare characters from start and end using two pointers.
 No extra memory or reverse function used.
 */
 
+import java.util.Scanner;
+
 public class palindromeCheck {
-    static boolean palindrome(String input) {
+    static boolean palindromeCheckForBothStringAndInteger(String input) {
         String removedSpaces = input.toLowerCase().replaceAll("\\s+", "");
         int left = 0;
-        int right = removedSpaces.length() - 1;
-
+        int right = removedSpaces.length();
         while (left < right) {
             if (input.charAt(left) != input.charAt(right)) {
                 return false;
@@ -29,8 +30,7 @@ public class palindromeCheck {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a String or Number: ");
         String input = scanner.nextLine();
-
-        boolean isPalindrome = palindrome(input);
+        boolean isPalindrome = palindromeCheckForBothStringAndInteger(input);
 
         if (isPalindrome) System.out.println(input + " is a palindrome");
         else System.out.println(input + " is NOT a palindrome");
